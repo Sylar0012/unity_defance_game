edb894eb689b2eb797bdd8551a5301d3ca587017
Sylar <10705min@gmail.com>
2025-05-02 19:29:59 +0900

    파일명 수정

---
+++ Assets/Scripts/PlantsScript.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class PlantsScript
+{
+    public int PlantsType;
+
+    private int _damage = 1;
+    
+    private int _health = 10;
+    
+    
+}
---
+++ Assets/Scripts/ZombiesScripts.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class ZombiesScripts
+
+{
+    public int ZombieType;
+
+    private int _damage = 1;
+    
+    private int _health = 10;
+    
+    
+}
